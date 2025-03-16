bffa6f2bf9affb0543fc0d2438da9910c39619c8
Ren Komatsu <ren.komatsu.rk@gmail.com>
2025-03-16 14:33:19 +0900

    Add flag to show version and help

--- app/main.go
+++ app/main.go
@@ -1,12 +1,19 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"io"
 	"log"
 	"os"
 
 	tea "github.com/charmbracelet/bubbletea"
+)
+
+var (
+	version = "dev"
+	commit  = "none"
+	date    = "unknown"
 )
 
 func main() {
@@ -22,26 +29,46 @@ func main() {
 		log.SetFlags(0)
 	}
 
+	// Parse arguments
+	versionFlag := flag.Bool("v", false, "Print version")
+	helpFlag := flag.Bool("h", false, "Print help")
+	flag.Parse()
+	args := flag.Args()
+
+	//
+	if *versionFlag {
+		fmt.Printf("Version: %s, Commit: %s, Build Date: %s\n", version, commit, date)
+		os.Exit(0)
+	}
+	//
+	if *helpFlag || len(args) >= 2 {
+		fmt.Println("Usage: jqcompletion [<filename>]")
+		fmt.Printf("Version [-v]: %s, Commit: %s, Build Date: %s\n", version, commit, date)
+		os.Exit(0)
+	}
+
+	// Main
 	jsonPath, err := getTempJsonPath()
 	if err != nil {
 		fmt.Print(err)
 		os.Exit(0)
 	}
 	defer os.Remove(jsonPath)
-	if len(os.Args) == 2 {
-		err := copyFile(os.Args[1], jsonPath)
+	if len(args) == 1 {
+		err := copyFile(args[0], jsonPath)
 		if err != nil {
 			fmt.Print(err)
 			os.Exit(0)
 		}
-	}
-	if len(os.Args) == 1 {
+	} else if len(args) == 0 {
 		cmd := editFileExecCmd(jsonPath)
 		err := cmd.Run()
 		if err != nil {
 			fmt.Print(err)
 			os.Exit(0)
 		}
+	} else {
+		panic("Wrong arguments")
 	}
 	m := initializeModel()
 	jsonData, err := LoadJsonFile(jsonPath)
